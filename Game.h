e632f459be0210ea96c19ae43f862990343ab094
Yuuuuu1725 <1319731941@qq.com>
2026-05-24 17:47:49 +0800

    Add files via upload

--- Game.h
+++ Game.h
@@ -1,107 +1,48 @@
-#pragma once
-#include "raylib.h"
+#ifndef GAME_H
+#define GAME_H
+
+#include <raylib.h>
 #include <vector>
-#include <memory>
 
-class Game;
-enum GameState { MENU, PLAYING, GAME_OVER };
-
-enum class PowerUpType {
-    PADDLE_EXTEND,
-    MULTI_BALL,
-    SLOW_BALL
-};
-
-struct Particle {
-    Vector2 pos;
-    Vector2 vel;
+struct Brick {
+    Rectangle rect;
+    bool destroyed;
+    int score;
     Color color;
-    float life;
 };
-
-class PowerUpEffect {
-public:
-    virtual void Apply(Game& game) = 0;
-    virtual ~PowerUpEffect() = default;
-};
-
-class ExtendPaddleEffect : public PowerUpEffect {
-public:
-    void Apply(Game& game) override;
-};
-
-class MultiBallEffect : public PowerUpEffect {
-public:
-    void Apply(Game& game) override;
-};
-
-class SlowBallEffect : public PowerUpEffect {
-public:
-    void Apply(Game& game) override;
-};
-
-std::unique_ptr<PowerUpEffect> CreatePowerUp(PowerUpType type);
 
 class Game {
 public:
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    struct Ball {
-        Vector2 pos;
-        Vector2 speed;
-        float radius;
-        bool active;
-    };
-
-    std::vector<Ball> balls;
-    Rectangle paddle;
-    float paddleSpeed;
-    float originalPaddleWidth;
-    std::vector<std::vector<bool>> destroyed;
-    int score;
-    int lives;
-    GameState currentState;
-    int currentLevel;
-    int brickRows;
-    bool infiniteLives;
-    Rectangle checkBoxRect;
-    Rectangle backButton;
-
-    struct PowerUpItem {
-        Vector2 pos;
-        PowerUpType type;
-        float speed;
-    };
-
-    std::vector<PowerUpItem> powerUps;
-    std::vector<Particle> particles;
-    const float POWERUP_DURATION = 10.0f;
-
-    bool hasPaddleExtend;
-    bool hasMultiBall;
-    bool hasSlowBall;
-    float paddleExtendTimer;
-    float multiBallTimer;
-    float slowBallTimer;
-    bool isSlowed;
-
-    bool isLoading;
-    float loadTimer;
-    float showLoadedTimer;
-
-public:
     Game();
     void Run();
-    void ResetGame();
+
+private:
+    void LoadLevel(int level);
     void ResetBall();
-    bool CheckAllBricksDestroyed();
-    void SpawnParticles(Vector2 pos, Color color);
-    void SpawnPowerUp(Vector2 pos);
-    void ExtendPaddle(float ratio);
-    void RestorePaddleSize();
-    void AddMultiBall();
-    void SlowDownBalls();
-    void RestoreBallSpeed();
-    void GenerateRandomBricks();
+    void ResetGame();
+    bool CheckWin();
+
+    void SaveProgress();
+    bool SaveExists(int lv);
+    void LoadSave(int lv);
+
+    int currentLevel;
+    int score;
+    int lives;
+
+    Rectangle paddle;
+    std::vector<Brick> bricks;
+    float ballX, ballY;
+    float ballDX, ballDY;
+
+    float paddleSpeed;
+    float ballSpeed;
+
+    enum GameState { MENU, PLAYING, GAME_OVER };
+    GameState currentState;
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 };
+
+#endif
